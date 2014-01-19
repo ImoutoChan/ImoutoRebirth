c0ab455891b53c79a845e56ca350c3cf96932ce2
ImoutoChan <kirirrin@gmail.com>
2014-01-19 17:16:57 +0400

    test1

--- ImageViewer/MainWindow.xaml.cs
+++ ImageViewer/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ namespace ImageViewer
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow 
     {
         #region Fields
 
