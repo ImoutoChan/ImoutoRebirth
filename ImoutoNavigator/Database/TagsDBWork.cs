ebea8cae76054e25ee9b1336820e0c2bc3c56294
ImoutoChan <kirirrin@gmail.com>
2014-04-01 18:04:54 +0400

    DB develop

--- ImoutoNavigator/Database/TagsDBWork.cs
+++ ImoutoNavigator/Database/TagsDBWork.cs
@@ -16,28 +16,27 @@ namespace ImoutoNavigator.Database
             //AddFile(@"C:\Users\oniii-chan\Pictures\[Leopard-Raws] Yozakura Quartet - Hana no Uta - 13 END (MX 1280x720 x264 AAC).mp4_snapshot_22.01_[2014.03.14_01.20.24].jpg");
         }
 
-        public void AddFile(string fullname)
-        {
-            var fi = new FileInfo(fullname);
-            if (!fi.Exists)
-            {
-                throw new ArgumentException("File does not exist.");
-            }
+        //public void AddFile(string fullname)
+        //{
+        //    var fi = new FileInfo(fullname);
+        //    if (!fi.Exists)
+        //    {
+        //        throw new ArgumentException("File does not exist.");
+        //    }
 
-            string md5 = Util.GetMd5Checksum(fi);
+        //    string md5 = Util.GetMd5Checksum(fi);
 
-            using (var obj = new TagsDBEntities())
-            {
-                obj.files.Add(new file()
-                              {
-                                  fullname = fullname,
-                                  md5 = md5
-                              });
-                obj.SaveChanges();
-            }
-        }
+        //    using (var obj = new TagsDBEntities())
+        //    {
+        //        obj.files.Add(new file()
+        //                      {
+        //                          fullname = fullname,
+        //                          md5 = md5
+        //                      });
+        //        obj.SaveChanges();
+        //    }
+        //}
+
+        public void   
     }
-
-
-
 }
