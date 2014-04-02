f798c9ca7f2e2b4813849f1654df297ef35bbe1e
ImoutoChan <kirirrin@gmail.com>
2014-04-02 09:48:20 +0400

    Getting methods for bd

--- ImoutoNavigator/Database/Model/ModelExtended.cs
+++ ImoutoNavigator/Database/Model/ModelExtended.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using ImoutoNavigator.Utils;
 
 namespace ImoutoNavigator.Database.Model
 {
+    public static class TagSetTypesEnum
+    {
+        public const string UserTypeName = "User";
+        public const string ActualTypeName = "Actual";
+    }
+
     public partial class Image
     {
+        #region Constructors
+
         public Image(string path) : this()
         {
             var fileInfo = new FileInfo(path);
@@ -18,5 +29,38 @@ namespace ImoutoNavigator.Database.Model
             Path = path;
             Size = fileInfo.Length;
         }
+
+        #endregion Constructors
+
+        #region Properties
+
+        public IEnumerable<TagSet> UserTagSets
+        {
+            get { return TagSets.Where(x => x.TagSetType.Name == TagSetTypesEnum.UserTypeName); }
+        }
+
+        public IEnumerable<TagSet> ActualTagSets
+        {
+            get { return TagSets.Where(x => x.TagSetType.Name == TagSetTypesEnum.ActualTypeName); }
+        }
+
+        #endregion Properties 
+    }
+
+    public partial class Tag
+    {
+        #region Properties
+
+        public IEnumerable<TagSet> UserTagSets
+        {
+            get { return TagSets.Where(x => x.TagSetType.Name == TagSetTypesEnum.UserTypeName); }
+        }
+
+        public IEnumerable<TagSet> ActualTagSets
+        {
+            get { return TagSets.Where(x => x.TagSetType.Name == TagSetTypesEnum.ActualTypeName); }
+        }
+
+        #endregion Properties 
     }
 }
--- ImoutoNavigator/Database/TagsDBWork.cs
+++ ImoutoNavigator/Database/TagsDBWork.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using ImoutoNavigator.Database.Model;
 using ImoutoNavigator.Utils;
 
 namespace ImoutoNavigator.Database
@@ -37,6 +39,55 @@ namespace ImoutoNavigator.Database
         //    }
         //}
 
-        public void   
+        public static IEnumerable<Image> GetImagesByTags(IEnumerable<Tag> tags)
+        {
+            var result = new List<Image>();
+            foreach (var tag in tags)
+            {
+                var relatedTagSets = tag.ActualTagSets.Concat(tag.UserTagSets);
+                foreach (var relatedTagSet in relatedTagSets)
+                {
+                    if (!result.Contains(relatedTagSet.Image))
+                    {
+                        result.Add(relatedTagSet.Image);
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static IEnumerable<Tag> GetTagsTopFromImages(IEnumerable<Image> images, int? count = null)
+        {
+            IEnumerable<Image> enumerableImages = images as IList<Image> ?? images.ToList();
+            if (!enumerableImages.Any())
+            {
+                throw new ArgumentException("Argument 'images' is empty.");
+            }
+
+            var result = enumerableImages
+                    .SelectMany(x => x
+                        .ActualTagSets
+                        .Union(x.UserTagSets)
+                        .SelectMany(y => y.Tags))
+                    .GroupBy(x => x.Id)
+                    .OrderBy(x => x.Count())
+                    .Select(x => x.First());
+
+            if (count != null)
+            {
+                result = result.Take(count.GetValueOrDefault());
+            }
+
+            return result;
+        }
+
+        public static IEnumerable<Tag> GetTagsFromImage(Image image)
+        {
+            return image
+                .UserTagSets
+                .Concat(image.ActualTagSets)
+                .Select(x => x.Tags)
+                .Aggregate((res, x) => res.Union(x).ToList());
+        }
     }
 }
