1370dd05ad6dc358936857e6febec0de771accf4
ImoutoChan <kirirrin@gmail.com>
2017-09-16 14:18:51 +0300

    Change ff add-on to webextension

---
+++ firefox/src-webextensions/background.js
@@ -0,0 +1,298 @@
+﻿var cachedMd5 = []; // md5, result (string: True, False, Relative)
+var processingMd5 = []; // md5, tabId, result, state = { init, processing, ready }
+var hideStatus = "enabled"; // enabled, disabled
+var listener = "http://localhost:60024/";
+var connection_state = "init"; //init, requested, connected
+var mainThreadInterval;
+var threadState = "init";
+
+function sync()
+{
+    if (connection_state == "init")
+    {
+        if (threadState == "connected") {
+            threadState = "init";
+            clearInterval(mainThreadInterval);
+            mainThreadInterval = setInterval(sync, 1000);
+        }
+        connect();
+    }
+    else 
+    {
+        if (threadState == "init")
+        {
+            threadState = "connected";
+            clearInterval(mainThreadInterval);
+            mainThreadInterval = setInterval(sync, 100);
+        }
+
+        //requestedReadyResponses
+        receiveReadyRequest();
+
+        //send needed
+        sendToRequestArray();
+    }
+
+    notifyTabs();
+}
+
+/* -- Remote Part -- */
+function connect()
+{
+    var postData = {
+        "action": "connectRequest"
+    };
+
+    console.log("----Connect request");
+    remotePost(listener, postData, function (response) {
+        //var result = JSON.parse(response);
+        var result = response;
+        if (result == "connectAccept")
+        {
+            connection_state = "connected";
+        }
+        console.log("----Connected");
+    });
+}
+
+function receiveReadyRequest() {
+
+    if (!processingMd5.some(function (element) { return element.state == "processing"; }))
+    {
+        return;
+    }
+
+    var postData = {
+        "action": "receiveReady"
+    };
+
+    $.post(listener, postData, function (response) {
+        var result = JSON.parse (response);
+        if (result.length > 0) {
+            console.log("Getted " + result.length + " results from native app");
+
+            for (var i = 0; i < result.length; i++)
+            {
+                for (var j = 0; j < processingMd5.length; j++)
+                {
+                    if (result[i].md5 == processingMd5[j].md5)
+                    {
+                        processingMd5[j].result = result[i].result;
+                        processingMd5[j].state = "ready";
+                    }
+                }
+
+                if (tryGetCached(result[i].md5) == null)
+                {
+                    cachedMd5.push({
+                        md5: result[i].md5,
+                        result: result[i].result
+                    });
+                }
+            }
+        }
+    }).fail(function () {
+        connection_state = "init";
+        console.log("----Connection lost");
+    });
+};
+
+function sendToRequestArray() {
+
+    var sendedArray = processingMd5.filter(function (el) { return el.state == "init"}).map(function (element) {
+        return {
+            md5: element.md5
+        }
+    });
+
+    if (sendedArray.length > 0) {
+
+        var json = JSON.stringify(sendedArray);
+
+        send("sendToRequest", json);
+
+        changeProcessingState(sendedArray, "processing");
+
+        console.log("Requested " + sendedArray.length + " hashes processing");
+    }
+};
+
+function send(action, data) {
+
+    var postData = {
+        "action": action,
+        "data": data
+    };
+
+    remotePost(listener, postData);
+};
+
+function notifyTabs() {
+
+    var notifyArray = processingMd5.filter(function (el) { return el.state == "ready"; });
+    if (notifyArray.length > 0) {
+        var tabsArray = {};
+        for (var i = 0; i < notifyArray.length; i++) {
+            var tabId = notifyArray[i].tabId;
+
+            if (!tabsArray[tabId]) {
+                tabsArray[tabId] = [];
+            }
+
+            tabsArray[tabId].push({ md5: notifyArray[i].md5, result: notifyArray[i].result });
+        }
+
+        var props = Object.getOwnPropertyNames(tabsArray);
+        for (var x = 0; x < props.length; x++) {
+            chrome.tabs.sendMessage(parseInt(props[x]), { md5array: tabsArray[props[x]], idCheck: "me", force: hideStatus });
+        }
+
+        removeProcessingHashes(notifyArray);
+
+        console.log("Sended " + notifyArray.length + " results to tabs")
+    }
+};
+
+function changeProcessingState(array, state)
+{
+    for (var i = 0; i < processingMd5.length; i++) {
+        if (array.some(function (el) { return processingMd5[i].md5 == el.md5; }))
+        {
+            processingMd5[i].state = state;
+        }
+    }
+};
+
+function removeProcessingHashes(array)
+{
+    for (var i = processingMd5.length - 1; i >= 0; i--) {
+        if (array.some(function (el) { return processingMd5[i].tabId == el.tabId && processingMd5[i].md5 == el.md5; })) {
+            processingMd5.splice(i, 1);
+        }
+    }
+}
+
+function tryGetCached(md5)
+{
+    var result = null;
+    for (var i = 0; i < cachedMd5.length; i++) {
+        if (cachedMd5[i].md5 == md5) {
+            result = cachedMd5[i];
+            break;
+        }
+    }
+    return result;
+}
+
+function remotePost(url, data, fresponse)
+{
+    $.ajax({
+        type: "POST",
+        url: listener,
+        data: data,
+        dataType: "json",
+        timeout: 100, // in milliseconds
+        success: fresponse,
+        error: function (request, status, err) {
+            connection_state = "init";
+            console.log("----Connection lost: " + status);
+        }
+    });
+}
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse)
+    {
+        var nativeCounter = 0;
+        var cachedCounter = 0;
+        if (request.idCheck == "me")
+        {
+            var md5s = request.md5sToProcess;
+            var tId = sender.tab.id;
+
+            var len = md5s.length;
+            for (var i = 0; i < len; i++) {
+
+                if (!processingMd5.some(function (el, index, array) { 
+                    return el.md5 == md5s[i] && el.tabId == tId;
+                }))
+                {
+                    var cachedValue = tryGetCached(md5s[i]);
+                    if (cachedValue != null)
+                    {
+                        processingMd5.push({
+                            tabId: tId,
+                            md5: md5s[i],
+                            state: "ready",
+                            result: cachedValue.result
+                        });
+                        cachedCounter++;
+                    }
+                    else
+                    {
+                        processingMd5.push({
+                            tabId: tId,
+                            md5: md5s[i],
+                            state: "init",
+                        });
+                        nativeCounter++
+                    }                    
+                }
+            }
+        }
+        else
+        {
+            if (request.idCheck == "link")
+            {
+                chrome.tabs.create({ url: request.link, active: false });
+            }
+
+        }
+
+        if (nativeCounter != 0 || cachedCounter != 0) {
+            console.log("Request on process from background: " + sender.tab.url
+                + "\nPrepared for native request: " + nativeCounter
+                + "\nFound in cache: " + cachedCounter);
+        }
+    });
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+    if (hideStatus == "enabled")
+    {
+        hideStatus = "disabled";
+        chrome.browserAction.setIcon({
+            path: {
+                19: "img/grayscale/icon19g.png",
+                38: "img/grayscale/icon38g.png"
+            }
+        });
+    }
+    else
+    {
+        hideStatus = "enabled";
+        chrome.browserAction.setIcon({
+            path: { 19: "img/normal/icon19.png",
+                    38: "img/normal/icon38.png"
+                }
+        });
+    }
+
+    chrome.tabs.query({}, function (tabs) {
+        for (var i = 0; i < tabs.length; ++i) {
+            chrome.tabs.sendMessage(tabs[i].id, { idCheck: "meSystem", force: hideStatus });
+        }
+    });
+});
+
+chrome.contextMenus.removeAll();
+chrome.contextMenus.create({
+    title: "Reset cache",
+    contexts: ["browser_action"],
+    onclick: function () {
+        cachedMd5 = [];
+    }
+});
+
+
+mainThreadInterval = setInterval(sync, 1000);
+sync();
---
+++ firefox/src-webextensions/content.js
@@ -0,0 +1,98 @@
+﻿var hashesResult = {};
+var hideEnabled = true;
+$("<style type='text/css'> .imoutoExtHide{ opacity: 0.6; outline: 3px solid red; outline-offset: -3px; } .imoutoExtHide:Hover{ opacity: 1; outline: 3px solid transparent; outline-offset: -3px;} .imoutoExtRelativeHide{ opacity: 0.6; outline: 3px solid green; outline-offset: -3px; } .imoutoExtRelativeHide:Hover{ opacity: 1; outline: 3px solid transparent; outline-offset: -3px;}</style>").appendTo("head");
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+    if (msg.idCheck && (msg.idCheck == "meSystem" || msg.idCheck == "me"))
+    {
+        if (msg.force == "enabled")
+        {
+            hideEnabled = true;
+        }
+        else
+        {
+            hideEnabled = false;
+        }
+    }
+
+    if (msg.idCheck && (msg.idCheck == "me")) {
+        var hashesProcessed = msg.md5array;
+
+        saveResults(hashesProcessed);
+    }
+    updateView();
+});
+
+function saveResults(array) {
+    for (var i = 0; i < array.length; i++) {
+        hashesResult[array[i].md5] = array[i].result;
+    }
+};
+
+function updateView() {
+    var result = new Array();
+    var relativeResults = new Array();
+    var antiresult = new Array();
+
+    var imgs = document.getElementsByTagName("img");
+
+    
+    for (var p in hashesResult) {
+        var regexp = new RegExp(p, "i");
+
+        for (var x = 0; x < imgs.length; x++) {
+            if (regexp.test(imgs[x].src)) {
+                if (hashesResult[p] == "True" && hideEnabled)
+                {                    
+                    result.push(imgs[x]);
+                }
+                else if (hashesResult[p] == "Relative" && hideEnabled)
+                {
+                    relativeResults.push(imgs[x]);
+                }
+                else
+                {
+                    antiresult.push(imgs[x]);
+                }
+            }            
+        }
+    }
+    
+    for (i = 0; i < result.length; i++) {
+        $(result[i]).removeClass("imoutoExtNoHide").addClass("imoutoExtHide");
+    }
+
+    for (i = 0; i < relativeResults.length; i++) {
+        $(relativeResults[i]).removeClass("imoutoExtNoHide").addClass("imoutoExtRelativeHide");
+    }
+
+    for (i = 0; i < antiresult.length; i++) {
+        $(antiresult[i]).removeClass("imoutoExtHide").removeClass("imoutoExtRelativeHide").addClass("imoutoExtNoHide");
+    }
+};
+
+function requestInfo()
+{
+    var imgs = document.getElementsByTagName("img");
+    var result = new Array();
+    for (var x = 0; x < imgs.length; x++) {
+        if (/\/[^\s"]*[a-zA-Z0-9]{32}(\.[a-zA-Z]*|\/)/ig.test(imgs[x].src)) {
+            result.push(imgs[x]);
+        }
+    }
+    var toSend = result.map(function (element) { return element.src.match(/[a-zA-Z0-9]{32}/i)[0]; })
+
+    var filteredToSend = toSend.filter(function (el) {
+        return !Object.getOwnPropertyNames(hashesResult).some(function (e) { return e == el; });
+    });
+
+    if (filteredToSend.length > 0) {
+        chrome.runtime.sendMessage({ idCheck: "me", md5sToProcess: filteredToSend });
+    }
+}
+
+$(document).bind("DOMSubtreeModified", function () {
+    requestInfo();
+})
+
+requestInfo();
