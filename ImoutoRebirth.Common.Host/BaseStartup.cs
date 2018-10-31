929adfae297480e8f0996f1d201cf6e58f446514
ImoutoChan <kirirrin@gmail.com>
2018-10-31 18:24:15 +0300

    Add project files.

---
+++ ImoutoRebirth.Common.Host/BaseStartup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ImoutoRebirth.Common.Host
+{
+    public abstract class BaseStartup
+    {
+        public IConfiguration Configuration { get; }
+
+        protected BaseStartup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public abstract void ConfigureServices(IServiceCollection services);
+    }
+}
