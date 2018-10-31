2929bd2022b3c89c72c1f9acc18efe5e636fc08f
ImoutoChan <kirirrin@gmail.com>
2018-10-31 19:23:52 +0300

    Add infrastructure interfaces

---
+++ ImoutoRebirth.Lilin.Core/Infrastructure/IFileTagRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ImoutoRebirth.Lilin.Core.Models;
+
+namespace ImoutoRebirth.Lilin.Core.Infrastructure
+{
+    public interface IFileTagRepository
+    {
+        Task Add(FileTag fileTag);
+    }
+}
---
+++ ImoutoRebirth.Lilin.Core/Infrastructure/ITagRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ImoutoRebirth.Lilin.Core.Models;
+
+namespace ImoutoRebirth.Lilin.Core.Infrastructure
+{
+    public interface ITagRepository
+    {
+        Task Add(Tag tag);
+    }
+}
