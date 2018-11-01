334807ab4db5a23fce6a98f7647c1f2239dfa22a
ImoutoChan <kirirrin@gmail.com>
2018-11-01 19:12:42 +0300

    Add infrastructure project

---
+++ ImoutoRebirth.Lilin.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ImoutoRebirth.Lilin.Core.Infrastructure;
+using ImoutoRebirth.Lilin.DataAccess;
+
+namespace ImoutoRebirth.Lilin.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly LilinDbContext _context;
+
+        public UnitOfWork(LilinDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+            => _context.SaveChangesAsync(cancellationToken);
+
+
+        public Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            _context
+        }
+
+        public void Dispose() => _context.Dispose();
+    }
+}
