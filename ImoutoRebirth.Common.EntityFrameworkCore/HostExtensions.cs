bea29d5690497a2dc6971c7cc1ef11ee16e32f78
ImoutoChan <kirirrin@gmail.com>
2019-01-18 01:54:11 +0300

    Add library for efcore

---
+++ ImoutoRebirth.Common.EntityFrameworkCore/HostExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ImoutoRebirth.Common.EntityFrameworkCore
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateIfNecessary<TDbContext>(this IHost host) 
+            where TDbContext : DbContext
+        {
+            var services = host.Services;
+
+            var logger = services.GetRequiredService<ILogger>();
+            var context = services.GetRequiredService<TDbContext>();
+
+            context.Database.Migrate();
+
+            var migrations = context.Database.GetAppliedMigrations();
+            foreach (var migration in migrations)
+                logger.LogInformation($"Migrated to {migration}");
+
+            return host;
+        }
+    }
+}
