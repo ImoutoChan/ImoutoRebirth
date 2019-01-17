c6b7a64852de70b4a4b48947ddb6d5083246ee5c
ImoutoChan <kirirrin@gmail.com>
2019-01-18 02:09:51 +0300

    Add migration extension for IWebHost

--- ImoutoRebirth.Common.EntityFrameworkCore/HostExtensions.cs
+++ ImoutoRebirth.Common.EntityFrameworkCore/HostExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -8,6 +9,26 @@ namespace ImoutoRebirth.Common.EntityFrameworkCore
     public static class HostExtensions
     {
         public static IHost MigrateIfNecessary<TDbContext>(this IHost host) 
+            where TDbContext : DbContext
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var logger = services.GetRequiredService<ILogger>();
+                var context = services.GetRequiredService<TDbContext>();
+
+                context.Database.Migrate();
+
+                var migrations = context.Database.GetAppliedMigrations();
+                foreach (var migration in migrations)
+                    logger.LogInformation($"Migrated to {migration}");
+
+                return host;
+            }
+        }
+
+        public static IWebHost MigrateIfNecessary<TDbContext>(this IWebHost host) 
             where TDbContext : DbContext
         {
             using (var scope = host.Services.CreateScope())
