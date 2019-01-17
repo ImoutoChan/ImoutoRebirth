aa84aa209cc58b541ced82c971417fe559e0c4c0
ImoutoChan <kirirrin@gmail.com>
2019-01-17 17:57:09 +0300

    Add MassTransit project

---
+++ ImoutoRebirth.Common.MassTransit/MassTransitRabbitMqHostingBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using GreenPipes;
+using GreenPipes.Configurators;
+using MassTransit;
+using MassTransit.RabbitMq.Extensions.Hosting.Contracts;
+using MassTransit.RabbitMq.Extensions.Hosting.Extensions;
+using MassTransit.RabbitMqTransport;
+
+namespace ImoutoRebirth.Common.MassTransit
+{
+    public static class MassTransitRabbitMqHostingBuilderExtensions
+    {
+        private static IMassTransitRabbitMqHostingBuilder AddDefaultConsumer<TConsumer, TCommand>(
+            this IMassTransitRabbitMqHostingBuilder builder)
+            where TCommand : class where TConsumer : class, IConsumer<TCommand>
+            => builder.ConsumeByConvention<TConsumer, TCommand>(GetRetryPolicy, GetReceiveEndpointConfigurator);
+
+        private static void GetReceiveEndpointConfigurator(IRabbitMqReceiveEndpointConfigurator configurator)
+        {
+            configurator.PrefetchCount = 1;
+        }
+
+        private static void GetRetryPolicy(IRetryConfigurator retryConfigurator)
+            => retryConfigurator.Intervals(TimeSpan.FromSeconds(30), TimeSpan.FromSeconds(60));
+    }
+}
