46ed518b93d52b3eb6e06ae925130db407a668cc
ImoutoChan <kirirrin@gmail.com>
2019-01-20 16:50:32 +0300

    Add WhenAll extension

---
+++ ImoutoRebirth.Common/TaskExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ImoutoRebirth.Common
+{
+    public static class TaskExtensions
+    {
+        public static Task WhenAll(this IEnumerable<Task> tasks)
+            => Task.WhenAll(tasks);
+    }
+}
