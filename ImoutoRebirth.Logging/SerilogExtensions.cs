335744feb1dabe06501f7e7e29b772f396d1c176
ImoutoChan <kirirrin@gmail.com>
2019-01-21 00:18:28 +0300

    Add Logger project with serilog default configs

---
+++ ImoutoRebirth.Logging/SerilogExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Events;
+
+namespace ImoutoRebirth.Logging
+{
+    public static class SerilogExtensions
+    {
+        private const string _fileTemplate 
+            = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] <s:{SourceContext}> {Message}{NewLine}{Exception}";
+        private const string _consoleTemplate 
+            = "[{Timestamp:HH:mm:ss} {Level:u3}] <s:{SourceContext}> {Message:lj}{NewLine}{Exception}";
+        
+
+        public static LoggerConfiguration WithoutDefaultLoggers(this LoggerConfiguration configuration)
+            => configuration.MinimumLevel.Verbose()
+                            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                            .MinimumLevel.Override("System", LogEventLevel.Warning);
+
+        public static LoggerConfiguration WithConsole(this LoggerConfiguration configuration)
+            => configuration.WriteTo.Console(
+                outputTemplate: _consoleTemplate,
+                restrictedToMinimumLevel: LogEventLevel.Verbose);
+
+        public static LoggerConfiguration WithAllRollingFile(
+            this LoggerConfiguration configuration,
+            string pathFormat = "logs/{Date}-all.log")
+            => configuration.WriteTo.RollingFile(
+                pathFormat: pathFormat,
+                outputTemplate: _fileTemplate,
+                restrictedToMinimumLevel: LogEventLevel.Verbose);
+
+        public static LoggerConfiguration WithInformationRollingFile(
+            this LoggerConfiguration configuration,
+            string pathFormat = "logs/{Date}-information.log")
+            => configuration.WriteTo.RollingFile(
+                pathFormat: pathFormat,
+                outputTemplate: _fileTemplate,
+                restrictedToMinimumLevel: LogEventLevel.Information);
+
+        public static LoggerConfiguration PatchWithConfiguration(
+            this LoggerConfiguration configuration,
+            IConfiguration appConfiguration)
+            => configuration.ReadFrom.Configuration(appConfiguration);
+    }
+}
---
+++ ImoutoRebirth.Logging/WebHostBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Core;
+
+namespace ImoutoRebirth.Logging
+{
+    public static class WebHostBuilderExtensions
+    {
+        public static IWebHostBuilder ConfigureSerilog(
+            this IWebHostBuilder webHostBuilder,
+            Action<LoggerConfiguration, IConfiguration> configureLogger = null)
+        {
+            webHostBuilder.ConfigureLogging((context, builder) =>
+            {
+                builder.ClearProviders();
+                builder.AddSerilog(
+                    dispose: true, 
+                    logger: GetSerilogLogger(context.Configuration, configureLogger));
+            });
+
+            return webHostBuilder;
+        }
+
+        private static Logger GetSerilogLogger(
+            IConfiguration configuration,
+            Action<LoggerConfiguration, IConfiguration> configureLogger)
+        {
+            var loggerBuilder = new LoggerConfiguration()
+                               .Enrich.FromLogContext();
+
+            configureLogger?.Invoke(loggerBuilder, configuration);
+
+            return loggerBuilder.CreateLogger();
+        }
+    }
+}
