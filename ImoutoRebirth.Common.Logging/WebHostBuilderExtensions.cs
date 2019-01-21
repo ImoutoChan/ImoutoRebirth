15d13d1dbe975e2c7c26eedd8bd3a23faa914256
ImoutoChan <kirirrin@gmail.com>
2019-01-21 04:08:30 +0300

    Add logging extension for IHostBuilder

--- ImoutoRebirth.Common.Logging/WebHostBuilderExtensions.cs
+++ ImoutoRebirth.Common.Logging/WebHostBuilderExtensions.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Core;
 
 namespace ImoutoRebirth.Common.Logging
 {
-    public static class WebHostBuilderExtensions
+    public static class HostBuilderExtensions
     {
         public static IWebHostBuilder ConfigureSerilog(
             this IWebHostBuilder webHostBuilder,
@@ -22,6 +23,21 @@ namespace ImoutoRebirth.Common.Logging
             });
 
             return webHostBuilder;
+        }
+
+        public static IHostBuilder ConfigureSerilog(
+            this IHostBuilder hostBuilder,
+            Action<LoggerConfiguration, IConfiguration> configureLogger = null)
+        {
+            hostBuilder.ConfigureLogging((context, builder) =>
+            {
+                builder.ClearProviders();
+                builder.AddSerilog(
+                    dispose: true, 
+                    logger: GetSerilogLogger(context.Configuration, configureLogger));
+            });
+
+            return hostBuilder;
         }
 
         private static Logger GetSerilogLogger(
