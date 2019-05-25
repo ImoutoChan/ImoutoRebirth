c633dc66a766dd15249d00c413e778649615dda1
ImoutoChan <kirirrin@gmail.com>
2019-05-25 15:16:56 +0300

    Add base entity for domain project

---
+++ ImoutoRebirth.Common.Domain/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ImoutoRebirth.Common.Domain
+{
+    public abstract class Entity
+    {
+        private readonly List<IDomainEvent> _events = new List<IDomainEvent>();
+
+        protected Entity()
+        {
+        }
+
+        public IReadOnlyCollection<IDomainEvent> Events => _events;
+
+        protected void Add(IDomainEvent domainEvent) => _events.Add(domainEvent);
+    }
+}
