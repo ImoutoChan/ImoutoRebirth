2ff1575a0fe6f74d7fb955fc85d5c995c86583f1
ImoutoChan <kirirrin@gmail.com>
2019-05-26 02:04:31 +0300

    Add DomainEventNotificationHandler

---
+++ ImoutoProject.Common.Cqrs/Events/DomainEventNotificationHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ImoutoRebirth.Common.Domain;
+using MediatR;
+
+namespace ImoutoProject.Common.Cqrs.Events
+{
+    public abstract class DomainEventNotificationHandler<TEvent> : INotificationHandler<EventWrapper<IDomainEvent>>
+        where TEvent : IDomainEvent
+    {
+        public Task Handle(EventWrapper<IDomainEvent> wrapper, CancellationToken cancellationToken)
+        {
+            if (!(wrapper.Wrapped is TEvent domainEvent))
+                return Task.CompletedTask;
+
+            return Handle(domainEvent, cancellationToken);
+        }
+
+        protected abstract Task Handle(TEvent domainEvent, CancellationToken cancellationToken);
+    }
+}
