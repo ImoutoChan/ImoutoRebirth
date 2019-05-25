5854189eb2d132976547937dca5e78dd287e2259
ImoutoChan <kirirrin@gmail.com>
2019-05-25 21:00:27 +0300

    Add implementation of EventStorage

---
+++ ImoutoRebirth.Common.Domain/EventStorage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ImoutoRebirth.Common.Domain
+{
+    public class EventStorage : IEventStorage
+    {
+        private readonly List<IDomainEvent> _events = new List<IDomainEvent>();
+
+        public void Add(IDomainEvent domainEvent) => _events.Add(domainEvent);
+
+        public IReadOnlyCollection<IDomainEvent> GetAll() => _events;
+    }
+}
