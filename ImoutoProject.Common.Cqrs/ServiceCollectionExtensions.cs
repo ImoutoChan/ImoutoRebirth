fe88a00a5aa4b524e7132efb1afe393cd8c7c912
ImoutoChan <kirirrin@gmail.com>
2019-05-26 19:11:15 +0300

    Add behavior registrations

---
+++ ImoutoProject.Common.Cqrs/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using ImoutoProject.Common.Cqrs.Behaviors;
+using ImoutoProject.Common.Cqrs.Events;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ImoutoProject.Common.Cqrs
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddTransactionBehavior(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(TransactionBehavior<,>));
+            services.AddTransient<IEventPublisher, EventPublisher>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddLoggingBehavior(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+
+            return services;
+        }
+    }
+}
