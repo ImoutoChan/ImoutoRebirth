993a0c3bea0953db2307b82aab88035e51afe165
ImoutoChan <kirirrin@gmail.com>
2019-05-31 21:31:41 +0300

    Add MessageContracts for receiving results of metadata update

---
+++ ImoutoRebirth.Meido.MessageContracts/INotesUpdatedCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ImoutoRebirth.Meido.MessageContracts
+{
+    public interface INotesUpdatedCommand
+    {
+        int SourceId { get; }
+
+        int[] PostIds { get; }
+
+        DateTimeOffset LastNoteUpdateDate { get; }
+    }
+}
