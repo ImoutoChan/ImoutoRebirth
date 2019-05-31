963e8a66ae70dd8df7ab36dc2611061387528ee5
ImoutoChan <kirirrin@gmail.com>
2019-05-31 22:56:48 +0300

    Add specifications to domain

---
+++ ImoutoRebirth.Common.Domain/Specifications/ParameterReplacer.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace ImoutoRebirth.Common.Domain.Specifications
+{
+    internal class ParameterReplacer : ExpressionVisitor
+    {
+        private readonly ParameterExpression _parameter;
+
+        protected override Expression VisitParameter(ParameterExpression node)
+            => base.VisitParameter(_parameter);
+
+        internal ParameterReplacer(ParameterExpression parameter)
+        {
+            _parameter = parameter;
+        }
+    }
+}
