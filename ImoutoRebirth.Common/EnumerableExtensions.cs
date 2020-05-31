4443b56fa72047de2bf14df2164c2c2108b3a274
ImoutoChan <kirirrin@gmail.com>
2020-05-31 22:28:18 +0300

    Add DistinctBy extension method

---
+++ ImoutoRebirth.Common/EnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImoutoRebirth.Common
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> selector)
+        {
+            var hashSet = new HashSet<TKey>();
+
+            foreach (var item in source)
+            {
+                var propertyValue = selector(item);
+                if (hashSet.Add(propertyValue))
+                    yield return item;
+            }
+        }
+    }
+}
