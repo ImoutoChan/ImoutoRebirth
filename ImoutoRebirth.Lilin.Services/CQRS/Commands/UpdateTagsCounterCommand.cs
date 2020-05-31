23e42aeba9485c5198cfb04b003e4b4558a82779
ImoutoChan <kirirrin@gmail.com>
2020-06-01 02:45:43 +0300

    Change isolation level for UpdateTagsCountersCommand

--- ImoutoRebirth.Lilin.Services/CQRS/Commands/UpdateTagsCounterCommand.cs
+++ ImoutoRebirth.Lilin.Services/CQRS/Commands/UpdateTagsCounterCommand.cs
@@ -4,7 +4,7 @@ using ImoutoRebirth.Common.Cqrs.Behaviors;
 
 namespace ImoutoRebirth.Lilin.Services.CQRS.Commands
 {
-    [CommandQuery(IsolationLevel.ReadCommitted)]
+    [CommandQuery(IsolationLevel.Serializable)]
     public class UpdateTagsCountersCommand : ICommand
     {
     }
