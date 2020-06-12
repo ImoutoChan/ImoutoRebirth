a83efbf22ffc6fda1e11d82d92f6c21c559389b2
ImoutoChan <kirirrin@gmail.com>
2020-06-12 16:32:32 +0300

    Add some logging

--- ImoutoRebirth.Meido.Core/FaultTolerance/FaultToleranceService.cs
+++ ImoutoRebirth.Meido.Core/FaultTolerance/FaultToleranceService.cs
@@ -1,20 +1,27 @@
 ﻿using System.Threading.Tasks;
 using ImoutoRebirth.Meido.Core.ParsingStatus;
+using Microsoft.Extensions.Logging;
 
 namespace ImoutoRebirth.Meido.Core.FaultTolerance
 {
     internal class FaultToleranceService : IFaultToleranceService
     {
         private readonly IParsingStatusRepository _parsingStatusRepository;
+        private readonly ILogger<FaultToleranceService> _logger;
 
-        public FaultToleranceService(IParsingStatusRepository parsingStatusRepository)
+        public FaultToleranceService(
+            IParsingStatusRepository parsingStatusRepository, 
+            ILogger<FaultToleranceService> logger)
         {
             _parsingStatusRepository = parsingStatusRepository;
+            _logger = logger;
         }
 
         public async Task RequeueFaults()
         {
             var faultedParsingStatuses = await _parsingStatusRepository.Find(new AllFaultedStatusesSpecification());
+
+            _logger.LogInformation("Requesting update after faulted for {Count} posts", faultedParsingStatuses.Count);
 
             foreach (var parsingStatus in faultedParsingStatuses)
             {
--- ImoutoRebirth.Meido.Services/FaultTolerance/FaultToleranceSettings.cs
+++ ImoutoRebirth.Meido.Services/FaultTolerance/FaultToleranceSettings.cs
@@ -2,7 +2,7 @@
 {
     public class FaultToleranceSettings
     {
-        public bool IsEnabled { get; set; } = true;
+        public bool IsEnabled { get; set; } = false;
 
         public int RepeatEveryMinutes { get; set; } = 60 * 24;
     }
