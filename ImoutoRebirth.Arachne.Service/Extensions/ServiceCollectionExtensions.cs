afb002a6d68218379ad217f1c693f981a145a441
ImoutoChan <kirirrin@gmail.com>
2021-01-04 02:13:52 +0300

    Update dependencies and up to net5.0

--- ImoutoRebirth.Arachne.Service/Extensions/ServiceCollectionExtensions.cs
+++ ImoutoRebirth.Arachne.Service/Extensions/ServiceCollectionExtensions.cs
@@ -5,6 +5,7 @@ using ImoutoRebirth.Common.MassTransit;
 using ImoutoRebirth.Lilin.MessageContracts;
 using ImoutoRebirth.Meido.MessageContracts;
 using Microsoft.Extensions.DependencyInjection;
+using ReceiverApp = ImoutoRebirth.Arachne.MessageContracts.ReceiverApp;
 
 namespace ImoutoRebirth.Arachne.Service.Extensions
 {
@@ -29,27 +30,31 @@ namespace ImoutoRebirth.Arachne.Service.Extensions
 
         public static ITrueMassTransitConfigurator AddArachneServicesForRabbit(
             this ITrueMassTransitConfigurator builder)
-            => builder.AddConsumer<EverywhereSearchMetadataCommandConsumer, IEverywhereSearchMetadataCommand>()
-                      .AddConsumer<YandereSearchMetadataCommandConsumer, IYandereSearchMetadataCommand>()
-                      .AddConsumer<DanbooruSearchMetadataCommandConsumer, IDanbooruSearchMetadataCommand>()
-                      .AddConsumer<SankakuSearchMetadataCommandConsumer, ISankakuSearchMetadataCommand>()
-                      .AddConsumer<LoadTagHistoryCommandConsumer, ILoadTagHistoryCommand>(
-                           configurator =>
-                           {
-                               configurator.PrefetchCount = 16;
-                               configurator.AutoDelete = true;
-                               configurator.Durable = false;
-                           })
-                      .AddConsumer<LoadNoteHistoryCommandConsumer, ILoadNoteHistoryCommand>(
-                           configurator =>
-                           {
-                               configurator.PrefetchCount = 16;
-                               configurator.AutoDelete = true;
-                               configurator.Durable = false;
-                           })
-                      .AddFireAndForget<IUpdateMetadataCommand>(Lilin.MessageContracts.ReceiverApp.Name)
-                      .AddFireAndForget<ISearchCompleteCommand>(Meido.MessageContracts.ReceiverApp.Name)
-                      .AddFireAndForget<INotesUpdatedCommand>(Meido.MessageContracts.ReceiverApp.Name)
-                      .AddFireAndForget<ITagsUpdatedCommand>(Meido.MessageContracts.ReceiverApp.Name);
+            => builder
+                .AddConsumer<EverywhereSearchMetadataCommandConsumer, IEverywhereSearchMetadataCommand>(
+                    ReceiverApp.Name)
+                .AddConsumer<YandereSearchMetadataCommandConsumer, IYandereSearchMetadataCommand>(ReceiverApp.Name)
+                .AddConsumer<DanbooruSearchMetadataCommandConsumer, IDanbooruSearchMetadataCommand>(ReceiverApp.Name)
+                .AddConsumer<SankakuSearchMetadataCommandConsumer, ISankakuSearchMetadataCommand>(ReceiverApp.Name)
+                .AddConsumer<LoadTagHistoryCommandConsumer, ILoadTagHistoryCommand>(
+                    ReceiverApp.Name,
+                    configurator =>
+                    {
+                        configurator.PrefetchCount = 16;
+                        configurator.AutoDelete = true;
+                        configurator.Durable = false;
+                    })
+                .AddConsumer<LoadNoteHistoryCommandConsumer, ILoadNoteHistoryCommand>(
+                    ReceiverApp.Name,
+                    configurator =>
+                    {
+                        configurator.PrefetchCount = 16;
+                        configurator.AutoDelete = true;
+                        configurator.Durable = false;
+                    })
+                .AddFireAndForget<IUpdateMetadataCommand>(Lilin.MessageContracts.ReceiverApp.Name)
+                .AddFireAndForget<ISearchCompleteCommand>(Meido.MessageContracts.ReceiverApp.Name)
+                .AddFireAndForget<INotesUpdatedCommand>(Meido.MessageContracts.ReceiverApp.Name)
+                .AddFireAndForget<ITagsUpdatedCommand>(Meido.MessageContracts.ReceiverApp.Name);
     }
 }
