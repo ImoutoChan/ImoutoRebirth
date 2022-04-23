3a8b51b17b4ed3dc24bfc959a533ce622217a17f
ImoutoChan <kirirrin@gmail.com>
2022-04-23 16:08:09 +0400

    Fix EnumFilter for nullable enums

--- ImoutoRebirth.Common.WebApi/EnumFilter.cs
+++ ImoutoRebirth.Common.WebApi/EnumFilter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.OpenApi.Any;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Extensions;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
@@ -9,30 +10,36 @@ public sealed class EnumFilter : ISchemaFilter
 {
     public void Apply(OpenApiSchema schema, SchemaFilterContext context)
     {
-        if (!IsEnum(context.Type, out var enumName)) 
+        if (!IsEnum(context.Type, out var enumName, out var enumType)) 
             return;
 
         var extension = new OpenApiObject
         {
-            ["name"] = new OpenApiString(enumName ?? context.Type.Name),
+            ["name"] = new OpenApiString(enumName),
             ["modelAsString"] = new OpenApiBoolean(false)
         };
 
         schema.AddExtension("x-ms-enum", extension);
-        schema.Enum = Enum.GetNames(context.Type).Select(x => new OpenApiString(x) as IOpenApiAny).ToList();
+        schema.Enum = Enum.GetNames(enumType).Select(x => new OpenApiString(x) as IOpenApiAny).ToList();
     }
 
-    private static bool IsEnum(Type t, out string? enumName)
+
+    private static bool IsEnum(
+        Type t,
+        [NotNullWhen(true)] out string? enumName,
+        [NotNullWhen(true)] out Type? trueEnumType)
     {
         if (t.IsEnum)
         {
             enumName = t.Name;
+            trueEnumType = t;
             return true;
         }
 
         var nullableType = Nullable.GetUnderlyingType(t);
         enumName = nullableType?.Name;
 
-        return nullableType != null && nullableType.IsEnum;
+        trueEnumType = nullableType;
+        return nullableType is { IsEnum: true };
     }
 }
