12094057016b89911e118762d5616c0edcf13eaf
ImoutoChan <kirirrin@gmail.com>
2022-11-02 13:26:40 +0400

    Enhance DisallowedStatusTransferHandler logging

--- ImoutoRebirth.Meido.Services/DomainEventHandlers/DisallowedStatusTransferHandler.cs
+++ ImoutoRebirth.Meido.Services/DomainEventHandlers/DisallowedStatusTransferHandler.cs
@@ -16,12 +16,13 @@ public class DisallowedStatusTransferHandler : DomainEventNotificationHandler<Di
     protected override Task Handle(DisallowedStatusTransfer domainEvent, CancellationToken cancellationToken)
     {
         _logger.LogWarning(
-            "Parsing status was trying to set to incorrect status {NewStatus} from {Status}. " +
-            "For file {FileId} in source {Source}.",
+            "Parsing status was trying to set to incorrect status {NewStatus} from {Status} for file {FileId} with md5 {Md5} in source {Source} with {FileIdFromSource}",
             domainEvent.NewStatus,
             domainEvent.Model.Status,
             domainEvent.Model.FileId,
-            domainEvent.Model.Source);
+            domainEvent.Model.Md5,
+            domainEvent.Model.Source,
+            domainEvent.Model.FileIdFromSource);
             
         return Task.CompletedTask;
     }
