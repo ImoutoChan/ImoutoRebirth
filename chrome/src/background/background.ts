6a0da91d8db520f2234ef892c1ce3e9ee4462b0c
ImoutoChan <kirirrin@gmail.com>
2023-06-04 20:36:09 +0400

    Update to new manifest and refactor

--- chrome/src/background/background.ts
+++ chrome/src/background/background.ts
@@ -1,115 +1,28 @@
-import oboe = require("oboe");
-import { HashResult, ImgStatus } from "../contentscript/contentscript";
+// toggle showOwnedMediaBorders
+chrome.storage.local.set({ showOwnedMediaBorders: true });
 
-export interface FoundFile {
-    md5: string;
-}
-
-export interface KekkaiResponse {
-    hash: string;
-    status: "NotFound" | "Present" | "RelativePresent";
-}
-
-const token = "";
-
-const remoteKekkai = "http://localhost:11303/FileStatus?token=" + token;
-let logEnabled = true;
-let disabledApp = false;
-let loadingCounter: number = 0;
-
-function log(obj: any): void {
-    if (logEnabled) {
-        console.log("Imouto Extension: " + obj.toString());
-    }
-}
-
-function getFromKekkai(hashes: string[], notifyTabId: number, timerKey: string) {
-    const body = JSON.stringify(hashes);
-    log("requestedHashes: " + body);
+chrome.action.onClicked.addListener(async () => {
+    const { showOwnedMediaBorders } = await chrome.storage.local.get("showOwnedMediaBorders");
     
-    var config = {
-        url: remoteKekkai,
-        method: "POST",
-        cached: false,
-        headers: { 'Content-Type': 'application/json'},
-        body: body
-    }            
-      const oboeService = oboe(config);
-      oboeService
-        .node('!.*', (response: KekkaiResponse) => {  
-            log("Kekkai result element: " + JSON.stringify(response));
-            
-            notifyTabs([response], notifyTabId);
-        })
-        .fail((errorReport: oboe.FailReason) => {
-            log("Kekkai fail: " + JSON.stringify(errorReport));
-            console.timeEnd(timerKey);
-         })
-         .done(() => {
-            console.timeEnd(timerKey);
-         })
-}
-
-// send md5 request to imouto server
-async function tryMd5(hashes: string[], notifyTabId: number) {
-    const timerKey = 'loading' + loadingCounter++;
-
-    console.time(timerKey);
-
-    getFromKekkai(hashes, notifyTabId, timerKey);
-};
- 
-function ToImgStatus(response: KekkaiResponse) : ImgStatus {
-    switch (response.status) {
-        case "NotFound":
-            return ImgStatus.None;
-        case "Present":
-            return ImgStatus.Contains;
-        case "RelativePresent":
-            return ImgStatus.Relative;
-    }
-}
-
-// send md5 results to tabs
-function notifyTabs(result: KekkaiResponse[], notifyTabId: number) {
-    const results = result.map(x => <HashResult>{ hash: x.hash, result: ToImgStatus(x)});
-
-    log("Notifying with: " + JSON.stringify(results));
-    chrome.tabs.sendMessage(notifyTabId, { action: "md5TryResponse", response: results});
-};
-
-// receive tabs messages
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.action === "md5Try") {
-            tryMd5(request.hashes, sender.tab.id);
-        }
-    });
-
-
-// disable app
-chrome.browserAction.onClicked.addListener(function (tab) {
-    if (!disabledApp) {
-        disabledApp = true;
-        chrome.browserAction.setIcon({
+    if (showOwnedMediaBorders) {
+        console.log('showOwnedMediaBorders: false');
+        chrome.storage.local.set({ showOwnedMediaBorders: false });
+        
+        chrome.action.setIcon({
             path: {
                 19: "assets/img/grayscale/icon19g.png",
                 38: "assets/img/grayscale/icon38g.png"
             }
         });
     } else {
-        disabledApp = false;
-        chrome.browserAction.setIcon({
+        console.log('showOwnedMediaBorders: true');
+        chrome.storage.local.set({ showOwnedMediaBorders: true });
+
+        chrome.action.setIcon({
             path: {
                 19: "assets/img/normal/icon19.png",
                 38: "assets/img/normal/icon38.png"
             }
         });
     }
-
-    chrome.tabs.query({}, tabs => {
-        for (const tab of tabs) {
-            chrome.tabs.sendMessage(tab.id, { action: "disableApp", state: disabledApp });
-        }
-    });
 });
--- chrome/src/contentscript/contentscript.ts
+++ chrome/src/contentscript/contentscript.ts
@@ -1,7 +1,5 @@
 import './contentscript.scss';
-
-const hashesResult: HashResult[] = [];
-let disableApp = false;
+import oboe = require("oboe");
 
 export class HashResult {
     public hash: string;
@@ -15,7 +13,34 @@ export enum ImgStatus {
     Requested
 }
 
-function requestImagesInfo(): void {
+export interface FoundFile {
+    md5: string;
+}
+
+export interface KekkaiResponse {
+    hash: string;
+    status: "NotFound" | "Present" | "RelativePresent";
+}
+
+
+const hashesResult: HashResult[] = [];
+const token = "";
+const remoteKekkai = "http://localhost:11303/FileStatus?token=" + token;
+
+let logEnabled = false;
+let loadingCounter: number = 0;
+let localShowOwnedMediaBorders = true;
+
+const log = (obj: any): void => {
+    if (logEnabled) {
+        console.log("Imouto Extension: " + obj.toString());
+    }
+};
+
+const requestImagesInfo = async (): Promise<void> => {
+    const { showOwnedMediaBorders } = await chrome.storage.local.get("showOwnedMediaBorders");
+    localShowOwnedMediaBorders = showOwnedMediaBorders == undefined ? true : showOwnedMediaBorders;
+    
     const imgsToRequest 
         = Array.from(document.getElementsByTagName("img"))
             .filter(x => /\/[^\s"]*[a-zA-Z0-9]{32}(\.[a-zA-Z0-9]*|\/)/ig.test(x.src))
@@ -23,17 +48,13 @@ function requestImagesInfo(): void {
             .filter(x => !hashesResult.map(x => x.hash).some(y => y === x));
 
     if (imgsToRequest.length > 0) {
-        // uncomment to enable result cache
-        //imgsToRequest.map(x => <HashResult>{hash: x, result: ImgStatus.Requested}).forEach(x => hashesResult.push(x))
-
-        chrome.runtime.sendMessage({ action: "md5Try", hashes: imgsToRequest });
+        await tryMd5(imgsToRequest, 0);
     }
-
     updateView();
-}
+};
 
-function saveResults(results: HashResult[]): void {
-    console.log(JSON.stringify(results));
+const saveResults = (results: HashResult[]): void => {
+    log(JSON.stringify(results));
 
     for (const result of results) {
         const existed = hashesResult.find(x => x.hash === result.hash);
@@ -44,11 +65,11 @@ function saveResults(results: HashResult[]): void {
             hashesResult.push(result);
         }
 
-        console.log(result.hash + " " + result.result);
+        log(result.hash + " " + result.result);
     }
 };
 
-function updateView(): void {
+const updateView = (): void => {
     const imgs = Array.from(document.getElementsByTagName("img"));
 
     for (const img of imgs) {
@@ -62,7 +83,7 @@ function updateView(): void {
         img.classList.remove("imoutoExtRelativeHide");
         img.classList.remove("imoutoExtNoHide");  
 
-        if (disableApp) {
+        if (!localShowOwnedMediaBorders) {
             continue;
         }
 
@@ -78,18 +99,62 @@ function updateView(): void {
                 break;
         }
     }
-
 };
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.action === "md5TryResponse") {
-            saveResults(request.response);
-        } else if (request.action === "disableApp") {
-            disableApp = request.state;
-        }
+const getFromKekkai = async (hashes: string[], notifyTabId: number, timerKey: string) => {
+    const body = JSON.stringify(hashes);
+    log("requestedHashes: " + body);
+    
+    const config = {
+        url: remoteKekkai,
+        method: "POST",
+        cached: false,
+        headers: { 'Content-Type': 'application/json'},
+        body: body
+    };            
+    const oboeService = oboe(config);
+
+    oboeService
+        .node('!.*', (response: KekkaiResponse) => {  
+            log("Kekkai result element: " + JSON.stringify(response));
+            
+            const results = [response].map(x => <HashResult>{ hash: x.hash, result: ToImgStatus(x)});
+            saveResults(results);
+            updateView();
+        })
+        .fail((errorReport: oboe.FailReason) => {
+            log("Kekkai fail: " + JSON.stringify(errorReport));
+            console.timeEnd(timerKey);
+            })
+            .done(() => {
+            console.timeEnd(timerKey);
+            })
+};
+
+const tryMd5 = async (hashes: string[], notifyTabId: number): Promise<void> => {
+    const timerKey = 'loading' + loadingCounter++;
+    console.time(timerKey);
+    await getFromKekkai(hashes, notifyTabId, timerKey);
+};
+ 
+const ToImgStatus = (response: KekkaiResponse) : ImgStatus => {
+    switch (response.status) {
+        case "NotFound":
+            return ImgStatus.None;
+        case "Present":
+            return ImgStatus.Contains;
+        case "RelativePresent":
+            return ImgStatus.Relative;
+    }
+};
+
+chrome.storage.onChanged.addListener((changes, area) => {
+    if (area === 'local' && changes.showOwnedMediaBorders) {
+        const showOwnedMediaBorders = Boolean(changes.showOwnedMediaBorders.newValue);
+        localShowOwnedMediaBorders = showOwnedMediaBorders == undefined ? true : showOwnedMediaBorders;
         updateView();
-    });
+    }
+});
 
 var observer = new MutationObserver(requestImagesInfo);
 observer.observe(document.body, {childList: true, subtree: true });
