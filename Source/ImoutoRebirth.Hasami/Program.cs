e62b0d5c269025c598158dcd8e661b30251e56bc
ImoutoChan <kirirrin@gmail.com>
2023-06-25 13:35:10 +0400

    Add SplitBasedOnFoundInBooru script

--- Source/ImoutoRebirth.Hasami/Program.cs
+++ Source/ImoutoRebirth.Hasami/Program.cs
@@ -1,34 +1,4 @@
-﻿using System.Security.Cryptography;
-using ImoutoRebirth.RoomService.WebApi.Client;
+﻿using ImoutoRebirth.Hasami;
 
-var sourcePath = new DirectoryInfo(@"F:\income\NEW\!playground\traps");
-var targetFoundPath = new DirectoryInfo(Path.Combine(sourcePath.FullName, "!found"));
-var targetMissPath = new DirectoryInfo(Path.Combine(sourcePath.FullName, "!miss"));
-
-var files = sourcePath.GetFiles();
-var hashes = files.Select(x => (File: x, MD5: GetMd5Checksum(x))).ToList();
-var hashesOnly = hashes.Select(x => x.MD5).ToList();
-
-// calculate md5
-
-var roomClient = new CollectionFilesClient("http://localhost:11301", new HttpClient());
-var found = await roomClient.SearchAsync(new CollectionFilesRequest(null, null, int.MaxValue, hashesOnly, null, 0));
-
-targetFoundPath.Create();
-targetMissPath.Create();
-
-foreach (var fileHash in hashes)
-{
-    var newPath = found.Any(x => x.Md5 == fileHash.MD5)
-        ? Path.Combine(targetFoundPath.FullName, fileHash.File.Name)
-        : Path.Combine(targetMissPath.FullName, fileHash.File.Name);
-    fileHash.File.MoveTo(newPath);
-}
-
-string GetMd5Checksum(FileInfo fileInfo)
-{
-    using var md5 = MD5.Create();
-    using var stream = fileInfo.OpenRead();
-
-    return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
-}
+//await Scripts.SplitFilesToTwoFoldersBasedOnSavedStatus();
+await Scripts.SplitBasedOnFoundInBooru();
---
+++ Source/ImoutoRebirth.Hasami/Scripts.cs
@@ -0,0 +1,106 @@
+﻿using System.Security.Cryptography;
+using Flurl.Http.Configuration;
+using Imouto.BooruParser;
+using Imouto.BooruParser.Implementations.Danbooru;
+using Imouto.BooruParser.Implementations.Gelbooru;
+using Imouto.BooruParser.Implementations.Sankaku;
+using Imouto.BooruParser.Implementations.Yandere;
+using ImoutoRebirth.RoomService.WebApi.Client;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace ImoutoRebirth.Hasami;
+
+internal static class Scripts
+{
+    public static async Task SplitBasedOnSaved()
+    {
+        var sourcePath = new DirectoryInfo(@"F:\income\NEW\!playground\traps");
+        var targetFoundPath = new DirectoryInfo(Path.Combine(sourcePath.FullName, "!found"));
+        var targetMissPath = new DirectoryInfo(Path.Combine(sourcePath.FullName, "!miss"));
+
+        var files = sourcePath.GetFiles();
+        var hashes = files.Select(x => (File: x, MD5: GetMd5Checksum(x))).ToList();
+        var hashesOnly = hashes.Select(x => x.MD5).ToList();
+
+        var roomClient = new CollectionFilesClient("http://localhost:11301", new HttpClient());
+        var found = await roomClient.SearchAsync(new CollectionFilesRequest(null, null, int.MaxValue, hashesOnly, null, 0));
+
+        targetFoundPath.Create();
+        targetMissPath.Create();
+
+        foreach (var fileHash in hashes)
+        {
+            var newPath = found.Any(x => x.Md5 == fileHash.MD5)
+                ? Path.Combine(targetFoundPath.FullName, fileHash.File.Name)
+                : Path.Combine(targetMissPath.FullName, fileHash.File.Name);
+            fileHash.File.MoveTo(newPath);
+        }
+    }
+
+    public static async Task SplitBasedOnFoundInBooru()
+    {
+        var sourcePath = new DirectoryInfo(@"C:\Users\Oniii-chan\Downloads\pepper0\!new\!pngs");
+        var targetFoundPath = new DirectoryInfo(Path.Combine(sourcePath.FullName, "!found"));
+        var targetMissPath = new DirectoryInfo(Path.Combine(sourcePath.FullName, "!miss"));
+
+        var files = sourcePath.GetFiles();
+        var hashes = files.Select(x => (File: x, MD5: GetMd5Checksum(x))).ToList();
+        var hashesOnly = hashes.Select(x => x.MD5).ToList();
+        
+        targetFoundPath.Create();
+        targetMissPath.Create();
+
+        int counter = 0;
+        foreach (var fileHash in hashes)
+        {
+            try
+            {
+                var found = await IsFoundInAnyBooru(fileHash.MD5);
+            
+                var newPath = found
+                    ? Path.Combine(targetFoundPath.FullName, fileHash.File.Name)
+                    : Path.Combine(targetMissPath.FullName, fileHash.File.Name);
+                
+                Console.WriteLine($"{fileHash.File.Name} - {found} {++counter}/{hashes.Count}");
+                
+                fileHash.File.MoveTo(newPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+
+    private static async Task<bool> IsFoundInAnyBooru(string md5)
+    {
+        var services = new ServiceCollection();
+        services.AddBooruParsers();
+        services.AddMemoryCache();
+        services.Configure<DanbooruSettings>(x =>
+        {
+        });
+        services.Configure<SankakuSettings>(x =>
+        {
+        });
+        var provider = services.BuildServiceProvider();
+
+        var booruLoaders = provider.GetRequiredService<IEnumerable<IBooruApiLoader>>();
+
+        var tasks = booruLoaders.Select(x => x.SearchAsync($"md5:{md5}"));
+        
+        var results = await Task.WhenAll(tasks);
+
+        return results.Any(x => x.Results.Any());
+    }
+
+    private static string GetMd5Checksum(FileInfo fileInfo)
+    {
+        using var md5 = MD5.Create();
+        using var stream = fileInfo.OpenRead();
+
+        return BitConverter.ToString(md5.ComputeHash(stream)).Replace("-", "").ToLower();
+    }
+}
