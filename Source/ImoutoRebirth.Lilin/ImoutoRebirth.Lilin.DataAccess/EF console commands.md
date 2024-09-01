29aad2cc12f13b3a7819cb2b0fcd9e0d7b6d5c3f
ImoutoChan <kirirrin@gmail.com>
2024-09-01 13:11:40 +0500

    Improve Lilin queries performance

---
+++ Source/ImoutoRebirth.Lilin/ImoutoRebirth.Lilin.DataAccess/EF console commands.md
@@ -0,0 +1,11 @@
+﻿```
+dotnet tool update --global dotnet-ef
+```
+
+```
+cd Source
+```
+
+```
+dotnet ef migrations add <NAME> --project ImoutoRebirth.Lilin/ImoutoRebirth.Lilin.DataAccess/ImoutoRebirth.Lilin.DataAccess.csproj --startup-project ImoutoRebirth.Lilin/ImoutoRebirth.Lilin.Host/ImoutoRebirth.Lilin.Host.csproj
+```
---
+++ Source/ImoutoRebirth.Lilin/ImoutoRebirth.Lilin.DataAccess/Migrations/20240901070752_AddIncludingFileIdIndexes.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ImoutoRebirth.Lilin.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIncludingFileIdIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                CREATE INDEX IF NOT EXISTS "IX_FileTags_TagId_Including_FileId"
+                ON public."FileTags" ("TagId") INCLUDE ("FileId");
+                """);
+
+            migrationBuilder.Sql(
+                """
+                CREATE INDEX IF NOT EXISTS "IX_FileTags_TagId_Value_Including_FileId"
+                ON public."FileTags" ("TagId", "Value") INCLUDE ("FileId");
+                """);
+
+            migrationBuilder.Sql(
+                """
+                CREATE INDEX IF NOT EXISTS "IX_Tags_Lower_Name"
+                ON public."Tags" (lower("Name"));
+                """);
+
+            migrationBuilder.Sql(
+                """
+                CREATE EXTENSION IF NOT EXISTS pg_trgm;
+                """);
+
+            migrationBuilder.Sql(
+                """
+                CREATE INDEX IF NOT EXISTS "IX_Tags_Lower_Name_GIN" ON "Tags" USING GIN (lower("Name") gin_trgm_ops);
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                DROP INDEX IF EXISTS "IX_Tags_Lower_Name_GIN";
+                """);
+
+            migrationBuilder.Sql(
+                """
+                DROP INDEX IF EXISTS "IX_Tags_Lower_Name";
+                """);
+
+            migrationBuilder.Sql(
+                """
+                DROP INDEX IF EXISTS "IX_FileTags_TagId_Value_Including_FileId";
+                """);
+
+            migrationBuilder.Sql(
+                """
+                DROP INDEX IF EXISTS "IX_FileTags_TagId_Including_FileId";
+                """);
+
+            migrationBuilder.Sql(
+                """
+                DROP EXTENSION IF EXISTS pg_trgm;
+                """);
+        }
+    }
+}
